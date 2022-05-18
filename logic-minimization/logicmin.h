9ca5d0151672241418b70e5c8125ce4c0082ac9e
kumaradm <72954373+kumaradm@users.noreply.github.com>
2022-05-18 16:55:08 +0700

    logic-minimization v1

--- logic-minimization/logicmin.h
+++ logic-minimization/logicmin.h
@@ -1,5 +1,6 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 typedef struct element
 {
@@ -8,6 +9,15 @@ typedef struct element
     int data;
     struct element *next;
 }element;
+
+typedef struct array
+{
+    element *head;
+    int rows;
+    int columns;
+    element *tail;
+    int nodecount;
+}array;
 
 typedef struct matrix
 {
@@ -18,24 +28,50 @@ typedef struct matrix
     int nodecount;
 }matrix;
 
+typedef struct minterm
+{
+    int paired;
+    int number_ones;
+    int group;
+    int number_pairs;
+    array paired_minterms;
+    array binary;
+    struct minterm *next;
+}minterm;
+
+typedef struct table
+{
+    int number_prime_implicants;
+    matrix arr;
+    matrix brr;
+    array minterm_counter;
+}table;
+
+void initialize_array(array *m, int r);
 void initialize_matrix(matrix *m, int r, int c);
-
+int find_data_array(array *m, int r);
+int find_data_matrix(matrix *m, int r, int c);
+void create_array(array *m, int d, int r);
 void create_matrix(matrix *m, int d, int r, int c);
+void add_minterms(int n);
+minterm* create_minterm(int n);
+void initialize_table();
+void pairing();
+void display_minterms();
+int possible_pair(minterm *m1, minterm *m2);
+void add_pair(minterm *m1, minterm *m2);
+minterm* create_pair(minterm *m1, minterm *m2);
+void add_to_table();
+int check_dont_care(int n);
+void display_table();
+void filtering();
+int number_implicants(int a, int *b);
+int find_max(int *n);
+void fill_binary(minterm *m1, minterm *m2, minterm *m3);
+void convert_binary_to_minterm(int n);
+void remove_minterm(int n);
 
-void print_matrix(matrix *m);
-
-void print_kmap(matrix *m);
-
-void initialize_kmap();
-
-matrix read_kmap(matrix *m);
-
-matrix* get_full_one(matrix *m);
-
-matrix* get_four_one(matrix *m);
-
-matrix* get_two_h_one(matrix *m);
-
-matrix* get_two_v_one(matrix *m);
-
-int find_data(matrix *m, int r, int c);
+minterm *minterms_cache,*paired_minterms_cache;
+array minterms_input,dont_cares_input;
+table implicants_table;
+int max_minterms,bits_size,max_group_1,max_group_2;
--- logic-minimization/main.c
+++ logic-minimization/main.c
@@ -2,18 +2,52 @@
 
 int main()
 {
-    matrix temp;
-    initialize_matrix(&temp,2,4);
-    create_matrix(&temp,1,1,1);
-    create_matrix(&temp,1,1,2);
-    create_matrix(&temp,1,1,3);
-    create_matrix(&temp,0,1,4);
-    create_matrix(&temp,1,2,1);
-    create_matrix(&temp,1,2,2);
-    create_matrix(&temp,0,2,3);
-    create_matrix(&temp,1,2,4);
+    int i,n,number_variables,number_minterms,number_dont_care;
+    char any_dont_care;
 
-    printf("KARNAUGH MAP\n");
-    print_kmap(&temp);
+    max_group_1=max_group_2=-1;
+    printf("Number of variables\t\t\t\t: ");
+    scanf("%d",&number_variables);
+    bits_size=number_variables;
+    max_minterms=(int)pow(2,bits_size);
+    initialize_array(&minterms_input,max_minterms);
+    initialize_array(&dont_cares_input,max_minterms);
+    printf("Number of minterms (Excluding DON'T CARE)\t: ");
+    scanf("%d",&number_minterms);
+
+    if(number_minterms==0) return 1;
+    for(i=0;i<max_minterms;i++)
+    {
+        create_array(&minterms_input,-1,i);
+        create_array(&dont_cares_input,-1,i);
+    }
+    printf("Enter the minterms (Not DON'T CARE): \n");
+    for(i=0;i<number_minterms;i++)
+    {
+        scanf("%d",&n);
+        create_array(&minterms_input,1,n);
+        add_minterms(n);
+    }
+    printf("Any DON'T CARE? (Y/N): ");
+    scanf("%s",&any_dont_care);
+    if(any_dont_care=='Y' || any_dont_care=='y')
+    {
+        printf("Enter the number of DON'T CARE: ");
+        scanf("%d",&number_dont_care);
+        printf("Enter the DON'T CARE MINTERMS: \n");
+        for(i=0;i<number_dont_care;i++)
+        {
+            scanf("%d",&n);
+            create_array(&dont_cares_input,1,n);
+            add_minterms(n);
+        }
+    }
+    (&implicants_table)->number_prime_implicants=0;
+    initialize_table();
+    pairing();
+    display_table();
+    printf("Simplified Boolean Expression:\n");
+    filtering();
+    printf("\n");
     return 0;
 }
